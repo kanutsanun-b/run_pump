25b2b095e6de03f638073f3866869f57243d743a
Kanutsanun Bouking <52057321+kanutsanun-b@users.noreply.github.com>
2020-12-28 11:30:26 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# run_pump
-To run 12v pump using DHT22 and BMP280
+﻿# Run_pump version 0.0.1
+!!!For Raspberry Pi zero, Automatic pump depen on humidity.
+
+## Installation
+```python
+git clone https://github.com/kanutsanun-b/run_pump.git
+
+#Pressure to Mean sea level ... QNH
+pip install pressure2qnh
+
+#BMP280
+git clone https://github.com/pimoroni/enviro-phat.git
+or
+sudo apt-get install python3-envirophat
+    "cd to the library directory, and run: sudo python3 setup.py install"
+or
+sudo pip3 install envirophat
+
+#DHT22
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+    "cd Adafruit_Python_DHT, and run: sudo python3 setup.py install"
+or
+sudo pip3 install Adafruit_DHT
+```
+
+## Usage
+```python
+Crontab...
+@reboot sleep 300 && /home/wwwjobs/clean-static-cache.sh
+
+index_rh = 47 #If Humidity is below than this value, pump will be started.
+logfile = 'C:/Users/kanut/OneDrive/Documents_KB/python/RaspiZero_project/testlog/'
+Latitude=10.72
+station_height=13.657
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+| ... | ... |
+| ------ | ------ |
+| email | kanutsanun.b@gmail.com |
+| Build README | https://dillinger.io/ |
--- run_project.py
+++ run_project.py
@@ -1,53 +1,114 @@
 import lcddriver
 import time
+import csv
+from datetime import datetime
+from pressure2qnh import CorrectPressure #sudo pip3 install pressure2qnh
 from envirophat import weather #sudo apt-get install python3-envirophat
 import Adafruit_DHT #sudo pip3 install Adafruit_DHT
 import RPi.GPIO as GPIO
+
+"""Temperature_sensor = degree celsiue
+Pressure_sensor = hPa(mbar)
+Latitude = decimal
+station_height = meters"""
+
+index_rh = 45 #If Humidity is below than this value, pump will be started.
+logfile = 'C:/Users/kanut/OneDrive/Documents_KB/python/RaspiZero_project/testlog/'
+Latitude=10.72
+station_height=13.657
+
 GPIO.setmode(GPIO.BCM) # set up BCM GPIO numbering
 
-#Setup GPIO
-pin_pump = 21
+
+# Setup GPIO
+pin_pump = 27
 pin_switch = 22
-pin_dht = 20
-GPIO.setup(pin_pump, GPIO.OUT)
+pin_dht = 21
+GPIO.setup(pin_pump, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(pin_switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#DHT22
+# DHT22
 sensor = Adafruit_DHT.DHT22
 
+# Setup data logger
+class Logger:
+    def __init__(self, temperature, humidity, qnh):
+        self.data_dict = {}
+        self.temperature = temperature
+        self.humidity = humidity
+        self.qnh = qnh
+
+    def collect_data(self):
+        ''' collect data and assign to class variable '''
+        self.data_dict['sensor'] = (datetime.now(), self.temperature, self.humidity, self.qnh)
+    
+    @staticmethod
+    def log_data(logfile, datestamp):
+        ''' log the data into csv files '''
+        for file, data in Logger.data_dict.items():
+            with open(logfile + file + str(datestamp) + '.csv', 'a+', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(data)
+
+# Setup lcd
 lcd = lcddriver.lcd()
 lcd.lcd_display_string("System is running", 1)
 lcd.lcd_display_string("Temp: ", 2)
 lcd.lcd_display_string("Rh: ", 3)
-lcd.lcd_display_string("Pump status: ", 4)
+lcd.lcd_display_string("Pump status: Auto", 4)
 
+# Setup toggle switch
 def toggle_switch(pin_switch):
     if GPIO.input(pin_switch):
-        GPIO.output(pin_pump, GPIO.HIGH)  # Turn motor on
-    else:
         lcd.lcd_display_string("Pump status: off", 4)
-        GPIO.output(pin_pump, GPIO.LOW)  # Turn motor off
+        time.sleep(3)
+        GPIO.output(pin_pump, GPIO.LOW)
+        lcd.lcd_display_string("Pump status: Auto", 4)
+    else:
+        lcd.lcd_display_string("Pump status: on", 4)
+        time.sleep(3)
+        GPIO.output(pin_pump, GPIO.HIGH)
  
 # listen for changing edge on toggle switch (both directions)
 # event will interrupt the program and call the toggleLine function
 GPIO.add_event_detect(pin_switch, GPIO.BOTH, callback=toggle_switch, bouncetime=300)
 
 try:
+    
+    lcd.lcd_display_string("Pump status: Auto", 4)
+    
     while True:
         
-        #BMP280
-        temp280 = weather.temperature()
-        pres280 = weather.pressure()
+        datestamp = datetime.now().date()
+        time.sleep(2)
         
+        #BMP280
+        pressure280 = weather.pressure()
+        temperature280 = weather.temperature()
+        obj = CorrectPressure(Temperature_sensor=temperature280, Pressure_sensor=pressure280, Latitude=Latitude, station_height=station_height)
+        qnh = obj.cal_qnh()
+        time.sleep(3)
+
+        #DHT22
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin_dht)
+        
+        #Data logger
+        logger = Logger(temperature, humidity, qnh)
+        logger.collect_data()
+        logger.log_data(logfile, datestamp)
+        
         if humidity is not None and temperature is not None:
             txt_temp = "Temp: {temperature:.2f}"
             txt_rh = "Rh: {humidity:.2f}"
             lcd.lcd_display_string(txt_temp.format(temperature=temperature), 2)
-            lcd.lcd_display_string(txt.rh.format(humidity=humidity), 3)
+            lcd.lcd_display_string(txt_rh.format(humidity=humidity), 3)
+            if humidity < index_rh:
+                time.sleep(3)
+                GPIO.output(pin_pump, GPIO.HIGH)
+                time.sleep(180)
+                GPIO.output(pin_pump, GPIO.LOW)
             time.sleep(10)
-        else:
-            print ('Failed to get reading. Try again!')
-            
-except KeyboardInterrupt:
+        
+except:
     GPIO.cleanup()
+        
